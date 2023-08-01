1f9eeb38f988ada6f46f96966565d3f979125351
jinyeong-afk <xoeh7002@naver.com>
2023-08-01 23:49:12 +0900

    [CREATE] #1 users Entity 생성

---
+++ src/main/java/com/example/demo/model/Users.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+}
