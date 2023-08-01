95c54bb3e7e49c9a8a493c6e7c497debe32c9fa1
jinyeong-afk <xoeh7002@naver.com>
2023-08-02 00:47:02 +0900

    [CREATE] #1 회원가입 controller 연결

---
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.requestObject.RequestSignUpUsers;
+import com.example.demo.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/users")
+public class UsersController {
+    private final UsersService usersService;
+
+    @PostMapping
+    public void signUp(@RequestBody RequestSignUpUsers requestSignUpUsers) {
+        usersService.addUser(requestSignUpUsers);
+    }
+}
--- src/main/java/com/example/demo/service/UserServiceIml.java
+++ src/main/java/com/example/demo/service/UserServiceIml.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceIml implements UsersService {
     private final UsersRepository usersRepository;
 
     public void addUser(RequestSignUpUsers requestSignUpUsers) {
