de9666d46b8c9b832db0ac2f1e55a9299cf701e3
jinyeong-afk <xoeh7002@naver.com>
2023-08-08 14:59:09 +0900

    [CREATE] #5 Boards Entity 생성

---
+++ src/main/java/com/example/demo/model/Boards.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="boards")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Boards implements Serializable {
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content", nullable = false)
+    private String content;
+}
