0338e3a1680785ed31621af54d7bec4140f33e9c
jinyeong-afk <xoeh7002@naver.com>
2023-08-08 15:34:20 +0900

    [CREATE] #5 게시글 작성 Controller 구현

---
+++ src/main/java/com/example/demo/controller/BoardsController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.CreateBoardsDTO;
+import com.example.demo.requestObject.RequestCreateBoards;
+import com.example.demo.service.BoardsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/boards")
+@RequiredArgsConstructor
+public class BoardsController {
+
+    private final BoardsService boardsService;
+
+    @PostMapping
+    public void createBoard(@RequestBody RequestCreateBoards requestCreateBoards,
+        Authentication authentication) {
+        boardsService.addBoard(CreateBoardsDTO.builder()
+            .email(authentication.getName())
+            .title(requestCreateBoards.getTitle())
+            .content(requestCreateBoards.getContent())
+            .build());
+    }
+}
