6d6b818f16ba83dcf91d0fd6eec9800e5d49a7b3
jinyeong-afk <xoeh7002@naver.com>
2023-08-08 17:34:44 +0900

    [CREATE] #7 게시글 목록 페이지네이션 controller 구현

--- src/main/java/com/example/demo/controller/BoardsController.java
+++ src/main/java/com/example/demo/controller/BoardsController.java
@@ -2,14 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.CreateBoardsDTO;
 import com.example.demo.requestObject.RequestCreateBoards;
+import com.example.demo.responseObject.ResponseReadAllBoards;
 import com.example.demo.service.BoardsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,5 +31,11 @@ public class BoardsController {
             .content(requestCreateBoards.getContent())
             .build());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseReadAllBoards> ReadAllBoards(@RequestParam(required = false) Integer pagination) {
+        if (pagination == null) pagination = 0;
+        return ResponseEntity.ok(boardsService.getAllBoards(pagination));
     }
 }
