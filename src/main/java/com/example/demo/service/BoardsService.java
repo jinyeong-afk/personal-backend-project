d69fdafbcb17c963bb698f978aed8dfd65d94e91
jinyeong-afk <xoeh7002@naver.com>
2023-08-08 17:33:39 +0900

    [CREATE] #7 게시글 목록 페이지네이션 service 구현

--- src/main/java/com/example/demo/service/BoardsService.java
+++ src/main/java/com/example/demo/service/BoardsService.java
@@ -2,7 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.dto.CreateBoardsDTO;
 import com.example.demo.requestObject.RequestCreateBoards;
+import com.example.demo.responseObject.ResponseReadAllBoards;
 
 public interface BoardsService {
     public void addBoard(CreateBoardsDTO createBoardsDTO);
+    public ResponseReadAllBoards getAllBoards(Integer pagination);
 }
